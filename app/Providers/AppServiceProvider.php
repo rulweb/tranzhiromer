81df80c69552aeb2f66ab3574388bf8fc1e094f7
Anton Shipulin <rul22rus@gmail.com>
2025-08-31 23:23:39 +0700

    add setting forceScheme

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -19,6 +20,11 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        // Force HTTPS scheme for all generated URLs when APP_URL uses https or in production.
+        $appUrl = (string) config('app.url');
+
+        if ($this->app->isProduction() || ($appUrl !== '' && str_starts_with($appUrl, 'https://'))) {
+            URL::forceScheme('https');
+        }
     }
 }
